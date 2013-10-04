82365c77bd1c1d690168010e18828403f55098a4
Kakee <1528@1528tekiMac-mini.local>
2013-10-04 20:40:52 +0800

    调整位置

--- SourceCode/Client/memeda/CCBProject/memeda/Resources/RightMsgBox.js
+++ SourceCode/Client/memeda/CCBProject/memeda/Resources/RightMsgBox.js
@@ -2,18 +2,6 @@ var RightMsgBox = function() {};
 
 RightMsgBox.prototype.onDidLoadFromCCB = function () {
     this.rootNode.animationManager.setCompletedAnimationCallback(this, this.onAnimationComplete);
-    
-       // Do Scale
-    var screenSize = cc.Director.getInstance().getWinSizeInPixels();
-    var screenWidth = screenSize.width > screenSize.height ? screenSize.height : screenSize.width;
-    var screenHeight = screenSize.width > screenSize.height ? screenSize.width : screenSize.height;
-
-    // 针对非iphone5屏幕做缩小适配
-    if(screenHeight / screenWidth < 1136/640)
-    {   
-        this.msgLayout.setScaleX(0.98);
-        this.msgLayout.setScaleY(0.98);
-    }
 };
 
 RightMsgBox.prototype.ShowMsg = function(url, onClose) {
