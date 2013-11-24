12e7f64faeeb624411f8e719e46a77cfca9a9024
monkeybad <leejustin@LeetekiMacBook-Pro.local>
2013-11-24 11:25:42 +0800

    增加了楼顶

---
+++ SourceCode/Client/memeda/memeda/Classes/CPreloadHelperAndroidVar.h
@@ -0,0 +1,14 @@
+//
+//  CPreloadHelperAndroidVar.h
+//  memeda
+//
+//  Created by Lee Justin on 13-11-23.
+//
+//
+
+#ifndef memeda_CPreloadHelperAndroidVar_h
+#define memeda_CPreloadHelperAndroidVar_h
+
+
+
+#endif
